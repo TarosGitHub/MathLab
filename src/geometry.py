44111f8f4dfe68d2a9994a71b034b5047574c9fa
Taro Hoshino <no.taro1990@gmail.com>
2022-02-18 19:23:12 +0900

    Changed class name of Point

--- src/geometry.py
+++ src/geometry.py
@@ -8,14 +8,22 @@ class WorldCoordinateSystem(CoordinateSystem):
     """ワールド座標系"""
     pass
 
-class Point:
-    """座標点"""
+class ThreeDimensionalRectangularCoordinateSystem(CoordinateSystem):
+    """3次元直交座標系"""
+    pass
 
-    def __init__(self, x=0.0, y=0.0, z=0.0, coordinate_system=WorldCoordinateSystem()):
+class Coordinates(ABC):
+    """座標"""
+    pass
+
+class ThreeDimensionalRectangularCoordinates(Coordinates):
+    """3次元直交座標"""
+
+    def __init__(self, x, y, z):
         self._x = x
         self._y = y
         self._z = z
-        self._coordinate_system = coordinate_system
+        self._coordinate_system = ThreeDimensionalRectangularCoordinateSystem()
 
     def __str__(self):
         return "({},{},{})".format(self._x, self._y, self._z)
