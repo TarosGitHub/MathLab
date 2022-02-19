b002e0f3de33757d409c309bed407cbfa5258f07
Taro Hoshino <no.taro1990@gmail.com>
2022-02-19 15:11:45 +0900

    Add 2dRectanglarCoordinates class

--- src/geometry.py
+++ src/geometry.py
@@ -8,6 +8,10 @@ class WorldCoordinateSystem(CoordinateSystem):
     """ワールド座標系"""
     pass
 
+class TwoDimensionalRectangularCoordinateSystem(CoordinateSystem):
+    """2次元直交座標系"""
+    pass
+
 class ThreeDimensionalRectangularCoordinateSystem(CoordinateSystem):
     """3次元直交座標系"""
     pass
@@ -15,6 +19,33 @@ class ThreeDimensionalRectangularCoordinateSystem(CoordinateSystem):
 class Coordinates(ABC):
     """座標"""
     pass
+
+class TwoDimensionalRectangularCoordinates(Coordinates):
+    """2次元直交座標"""
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+        self._coordinate_system = TwoDimensionalRectangularCoordinateSystem()
+
+    def __str__(self):
+        return "({},{})".format(self._x, self._y)
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
 
 class ThreeDimensionalRectangularCoordinates(Coordinates):
     """3次元直交座標"""
@@ -51,4 +82,3 @@ class ThreeDimensionalRectangularCoordinates(Coordinates):
     @z.setter
     def z(self, value):
         self._z = value
-
